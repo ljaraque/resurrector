f6ab603b4971f76ec082872e15bd2d9fc89b739e
pi <you@example.com>
2016-11-21 17:26:46 +0000

    fixed http_server file name

--- monitor_http_resurrector.sh
+++ monitor_http_resurrector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until /usr/bin/python /home/pi/rfid_standalone/ugen/ugen.py 0.0.0.0; do 
+until /usr/bin/python /home/pi/resurrector/http_server.py 0.0.0.0; do 
     #echo " Server crashed with coe $?. Respawning..">&2
     sleep 1
 done
