dbdee4ba91ee8b6e55ae94f184ec89d3fd419b08
pi <you@example.com>
2016-11-22 04:01:56 +0000

    added PATH to resurrector.py

--- resurrector.py
+++ resurrector.py
@@ -2,13 +2,15 @@ import os.path
 import time
 import relay
 
+PATH = "/home/pi/resurrector/"
+
 gpio_to_other_machine_reset = 27
 now = time.time()
-if os.path.exists("/home/pi/resurrector/last_heartbeat.txt"):
-    with open('last_heartbeat.txt', 'r') as f:
+if os.path.exists(PATH + "last_heartbeat.txt"):
+    with open(PATH + 'last_heartbeat.txt', 'r') as f:
         time_of_last_heartbeat = f.readline()
 else:
-    with open("last_heartbeat.txt", "w") as text_file:
+    with open(PATH + "last_heartbeat.txt", "w") as text_file:
         text_file.write("%s" % now)
 print ( now - float(time_of_last_heartbeat) )/60
 if now - float(time_of_last_heartbeat) > 5 * 60:
