c6db85a855fc4f61da391273a00a5f97afa48ec0
pi <you@example.com>
2016-11-22 05:29:31 +0000

    added checking network before to reset other

--- resurrector.py
+++ resurrector.py
@@ -1,8 +1,10 @@
 import os.path
+from os import system
 import time
 import relay
 
 PATH = "/home/pi/resurrector/"
+GATEWAY = "10.1.1.100"
 
 gpio_to_other_machine_reset = 27
 now = time.time()
@@ -13,5 +15,5 @@ else:
     with open(PATH + "last_heartbeat.txt", "w") as text_file:
         text_file.write("%s" % now)
 print ( now - float(time_of_last_heartbeat) )/60
-if now - float(time_of_last_heartbeat) > 5 * 60:
+if now - float(time_of_last_heartbeat) > 5 * 60 and system("ping -c 1 " + GATEWAY) == 0: #check alive but if network is up
     relay.activate(gpio_to_other_machine_reset)
